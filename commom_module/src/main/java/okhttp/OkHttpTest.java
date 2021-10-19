f40bb1457bf95150c804cf471484604fb4fdfd58
liangdepeng <liangdepeng@wahaha.com.cn>
2021-10-19 09:15:51 +0800

    补充 Glide 4.12.0 源码

---
+++ commom_module/src/main/java/okhttp/OkHttpTest.java
@@ -0,0 +1,74 @@
+package okhttp;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by ldp.
+ * <p>
+ * Date: 2021/10/18
+ * <p>
+ * Summary:
+ */
+public class OkHttpTest {
+
+    private final ExecutorService executorService;
+
+    public OkHttpTest() {
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    // 同步请求
+    public void syncGet() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor()
+//                .addNetworkInterceptor()
+//                .connectTimeout()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://www.baidu.com")
+                .get()//.post(body)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response = call.execute();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    // 异步请求
+    public void asyncGet() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://www.baidu.com")
+                .get()//.post(body)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+            }
+        });
+    }
+}
